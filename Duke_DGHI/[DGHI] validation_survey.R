0395c20ab0b38758b23443d8a75dc25a7ab0b4d4
joaovissoci <joaovissoci@gmail.com>
2016-01-08 07:41:55 -0500

    updating from notebook on jan08

--- Duke_DGHI/[DGHI] validation_survey.R
+++ Duke_DGHI/[DGHI] validation_survey.R
@@ -15,22 +15,36 @@
 #install.packages("ggplot2", repos="http://cran.r-project.org")
 
 #command below will install each package. if you run this script from the beginning you need to run every single one again
-lapply(c("epicalc", "sem","Hmisc","ggplot2", "psych", "RCurl","irr", "nortest", "moments","GPArotation","Kendall","nFactors","repmis","gdata","vcd","polycor","ROCR","pROC","vcd","psy"), library, character.only=T)
+lapply(c("epicalc", "sem","Hmisc","ggplot2", "psych","irr", "nortest", "moments","GPArotation","Kendall","nFactors","repmis","gdata","vcd","polycor","ROCR","pROC","vcd","psy"), library, character.only=T)
 
 ########################################################
 #Datasets
 ########################################################
 
 #if you are using a file that is local to your computer, then replace path below by path to the data file. command will throw all the data into the templateData object. replace the word template.data by a name that might easier for you to remember and that represents your data
-data_cluster_police <- read.csv("/Users/joaovissoci/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/POL_RWA_severity.csv", sep=",")
+data_cluster_police_rw <- read.csv("/Users/joaovissoci/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/POL_RWA_severity.csv", sep=",")
 
-data_cluster_survey <- read.csv("/Users/joaovissoci/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/SURVEY_RWA_severity.csv",sep=",")
+data_cluster_survey_rw <- read.csv("/Users/joaovissoci/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/SURVEY_RWA_severity.csv",sep=",")
 
 
 #if you are using a file that is local to your computer, then replace path below by path to the data file. command will throw all the data into the templateData object. replace the word template.data by a name that might easier for you to remember and that represents your data
-data_cluster_police <- read.csv("/Users/joaovissoci/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/police_SL_1612.csv",sep=",")
+data_cluster_police_sl <- read.csv("/Users/joaovissoci/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/police_SL_1612.csv",sep=",")
 
-data_cluster_survey <- read.csv("/Users/joaovissoci/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/survey_sl_1612.csv",sep=",")
+data_cluster_survey_sl <- read.csv("/Users/joaovissoci/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/survey_sl_1612.csv",sep=",")
+
+
+#if you are using a file that is local to your computer, then replace path below by path to the data file. command will throw all the data into the templateData object. replace the word template.data by a name that might easier for you to remember and that represents your data
+data_cluster_police_rw <- read.csv("/home/joao/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/POL_RWA_severity.csv", sep=",")
+
+data_cluster_survey_rw <- read.csv("/home/joao/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/SURVEY_RWA_severity.csv",sep=",")
+
+
+#if you are using a file that is local to your computer, then replace path below by path to the data file. command will throw all the data into the templateData object. replace the word template.data by a name that might easier for you to remember and that represents your data
+data_cluster_police_sl <- read.csv("/home/joao/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/police_SL_1612.csv",sep=",")
+
+data_cluster_survey_sl <- read.csv("/home/joao/Dropbox/datasets/DGHI/Africa_DGHI/surveyXpolice_validation/survey_sl_1612.csv",sep=",")
+
+###
 
 ########################################################
 #Agreemente Rwuanda data 
@@ -38,29 +52,29 @@ data_cluster_survey <- read.csv("/Users/joaovissoci/Dropbox/datasets/DGHI/Africa
 
 #Recoding police data kernel density into low and high risk areas
 ##Adjusting NA's
-data_cluster_police$RISK<-data_cluster_police$MEANpolice
-data_cluster_police$RISK<-car::recode(data_cluster_police$RISK,"NA=0")
+data_cluster_police_rw$RISK<-data_cluster_police_rw$MEANpolice
+data_cluster_police_rw$RISK<-car::recode(data_cluster_police_rw$RISK,"NA=0")
 ##Calculate tertiles
-#quantile(data_cluster_police$RISK, 0:4/4)
-#data_cluster_police$RISK<-car::recode(data_cluster_police$RISK,"0.0000:1049.274=0;else=1")
-data_cluster_police$RISK<-car::recode(data_cluster_police$RISK,"0.0000:349.758000=0;349.758001:1049.274=1;else=2")
-#data_cluster_police$RISK<-as.factor(data_cluster_police$RISK)
+#quantile(data_cluster_police_rw$RISK, 0:4/4)
+#data_cluster_police_rw$RISK<-car::recode(data_cluster_police_rw$RISK,"0.0000:1049.274=0;else=1")
+data_cluster_police_rw$RISK<-car::recode(data_cluster_police_rw$RISK,"0.0000:349.758000=0;349.758001:1049.274=1;else=2")
+#data_cluster_police_rw$RISK<-as.factor(data_cluster_police_rw$RISK)
 #Recoding survey data kernel density into low and high risk areas
-data_cluster_survey$RISK<-data_cluster_survey$MEANsurvey
-data_cluster_survey$RISK<-car::recode(data_cluster_survey$RISK,"NA=0")
-data_cluster_survey$RISK<-car::recode(data_cluster_survey$RISK,"0.0000:103.818400=0;103.818401:311.455200=1;else=2")
-#data_cluster_survey$RISK<-as.factor(data_cluster_survey$RISK)
+data_cluster_survey_rw$RISK<-data_cluster_survey_rw$MEANsurvey
+data_cluster_survey_rw$RISK<-car::recode(data_cluster_survey_rw$RISK,"NA=0")
+data_cluster_survey_rw$RISK<-car::recode(data_cluster_survey_rw$RISK,"0.0000:103.818400=0;103.818401:311.455200=1;else=2")
+#data_cluster_survey_rw$RISK<-as.factor(data_cluster_survey_rw$RISK)
 
-agree_data_rw<-data.frame(data_cluster_police$RISK,data_cluster_survey$RISK)
+agree_data_rw<-data.frame(data_cluster_police_rw$RISK,data_cluster_survey_rw$RISK)
 #agree_data_rw<-na.omit(agree_data_rw)
-#agree_data_rw$data_cluster_police.RISK<-car::recode(agree_data_rw$data_cluster_police.RISK,"1=0;2=1;3=1")
-#agree_data_rw$data_cluster_survey.RISK<-car::recode(agree_data_rw$data_cluster_survey.RISK,"1=0;2=1;3=1")
+#agree_data_rw$data_cluster_police_rw.RISK<-car::recode(agree_data_rw$data_cluster_police_rw.RISK,"1=0;2=1;3=1")
+#agree_data_rw$data_cluster_survey_rw.RISK<-car::recode(agree_data_rw$data_cluster_survey_rw.RISK,"1=0;2=1;3=1")
 #Executing agreement nalysis
 agree<-agree(agree_data_rw, tolerance=0) #% of Agreement
 kappa<-cohen.kappa(agree_data_rw) #Kappa-value
 AC1(kappa$agree)
 #cor<-cor(agree_data_rw,method=c("kendall"))
-#kendall<-Kendall(agree_data_rw$data_cluster_police.RISK,agree_data_rw$data_cluster_survey.RISK)
+#kendall<-Kendall(agree_data_rw$data_cluster_police_rw.RISK,agree_data_rw$data_cluster_survey_rw.RISK)
 #poly<-hetcor(agree_data_rw)
 
 obj1_rw<-ckappa(agree_data_rw)
@@ -72,7 +86,7 @@ agree_plot_rw<-agreementplot(obj1_rw$table, main = "Rwanda",xlab_rot=0, ylab_rot
 agree_data_rw[,1]<-car::recode(agree_data_rw[,1],"0=0;else=1")
 agree_data_rw[,2]<-car::recode(agree_data_rw[,2],"0=0;else=1")
 
-agree_data_rw<-with(agree_data_rw,data.frame(as.numeric(data_cluster_police.RISK),as.numeric(data_cluster_survey.RISK)))
+agree_data_rw<-with(agree_data_rw,data.frame(as.numeric(data_cluster_police_rw.RISK),as.numeric(data_cluster_survey_rw.RISK)))
 
 pred <- prediction(agree_data_rw[,1],agree_data_rw[,2])
 
@@ -92,29 +106,29 @@ plot(perf1)
 ########################################################
 #Recoding police data kernel density into low and high risk areas
 ##Adjusting NA's
-data_cluster_police$RISK<-data_cluster_police$MEANpolice
-data_cluster_police$RISK<-car::recode(data_cluster_police$RISK,"NA=0")
+data_cluster_police_sl$RISK<-data_cluster_police_sl$MEANpolice
+data_cluster_police_sl$RISK<-car::recode(data_cluster_police_sl$RISK,"NA=0")
 ##Calculate tertiles
-#quantile(data_cluster_police$RISK, 0:4/4)
-#data_cluster_police$RISK<-car::recode(data_cluster_police$RISK,"0.0000:1049.274=0;else=1")
-data_cluster_police$RISK<-car::recode(data_cluster_police$RISK,"0.0000:67.823400=0;67.823401:203.470200=1;else=2")
-#data_cluster_police$RISK<-as.factor(data_cluster_police$RISK)
+#quantile(data_cluster_police_sl$RISK, 0:4/4)
+#data_cluster_police_sl$RISK<-car::recode(data_cluster_police_sl$RISK,"0.0000:1049.274=0;else=1")
+data_cluster_police_sl$RISK<-car::recode(data_cluster_police_sl$RISK,"0.0000:67.823400=0;67.823401:203.470200=1;else=2")
+#data_cluster_police_sl$RISK<-as.factor(data_cluster_police_sl$RISK)
 #Recoding survey data kernel density into low and high risk areas
-data_cluster_survey$RISK<-data_cluster_survey$MEANsurvey
-data_cluster_survey$RISK<-car::recode(data_cluster_survey$RISK,"NA=0")
-data_cluster_survey$RISK<-car::recode(data_cluster_survey$RISK,"0.0000:87.445400=0;87.445401:262.336200=1;else=2")
-#data_cluster_survey$RISK<-as.factor(data_cluster_survey$RISK)
+data_cluster_survey_sl$RISK<-data_cluster_survey_sl$MEANsurvey
+data_cluster_survey_sl$RISK<-car::recode(data_cluster_survey_sl$RISK,"NA=0")
+data_cluster_survey_sl$RISK<-car::recode(data_cluster_survey_sl$RISK,"0.0000:87.445400=0;87.445401:262.336200=1;else=2")
+#data_cluster_survey_sl$RISK<-as.factor(data_cluster_survey_sl$RISK)
 
-agree_data_sl<-data.frame(data_cluster_police$RISK,data_cluster_survey$RISK)
+agree_data_sl<-data.frame(data_cluster_police_sl$RISK,data_cluster_survey_sl$RISK)
 #agree_data_sl<-na.omit(agree_data_sl)
-#agree_data_sl$data_cluster_police.RISK<-car::recode(agree_data_sl$data_cluster_police.RISK,"1=0;2=1;3=1")
-#agree_data_sl$data_cluster_survey.RISK<-car::recode(agree_data_sl$data_cluster_survey.RISK,"1=0;2=1;3=1")
+#agree_data_sl$data_cluster_police_sl.RISK<-car::recode(agree_data_sl$data_cluster_police_sl.RISK,"1=0;2=1;3=1")
+#agree_data_sl$data_cluster_survey_sl.RISK<-car::recode(agree_data_sl$data_cluster_survey_sl.RISK,"1=0;2=1;3=1")
 #Executing agreement nalysis
 agree<-agree(agree_data_sl, tolerance=0) #% of Agreement
 kappa<-cohen.kappa(agree_data_sl) #Kappa-value
 AC1(kappa$agree)
 #cor<-cor(agree_data_sl,method=c("kendall"))
-#kendall<-Kendall(agree_data_sl$data_cluster_police.RISK,agree_data_sl$data_cluster_survey.RISK)
+#kendall<-Kendall(agree_data_sl$data_cluster_police_sl.RISK,agree_data_sl$data_cluster_survey_sl.RISK)
 #poly<-hetcor(agree_data_sl)
 
 obj1_sl<-ckappa(agree_data_sl)
@@ -125,7 +139,7 @@ agreementplot(obj1_sl$table, main = "Sri-Lanka",xlab_rot=0, ylab_rot=90,xlab_jus
 agree_data_sl[,1]<-car::recode(agree_data_sl[,1],"0=0;else=1")
 agree_data_sl[,2]<-car::recode(agree_data_sl[,2],"0=0;else=1")
 
-agree_data_sl<-with(agree_data_sl,data.frame(as.numeric(data_cluster_police.RISK),as.numeric(data_cluster_survey.RISK)))
+agree_data_sl<-with(agree_data_sl,data.frame(as.numeric(data_cluster_police_sl.RISK),as.numeric(data_cluster_survey_sl.RISK)))
 
 pred <- prediction(agree_data_sl[,1],agree_data_sl[,2])
 
@@ -191,17 +205,20 @@ poly<-polychoric(agree_data)
 #PLOTS FOR THE PAPER
 ##############################################################################
 
-tiff("/Users/joaovissoci/Desktop/figure2A.tiff", width = 700, height = 500,compression = 'lzw')
+tiff("/home/joao/Desktop/figure2A.tiff", units='in', width = 7, height = 6,compression = 'lzw',res=1200,bg = "white")
+postscript("/home/joao/Desktop/figure2A.eps",width = 1200, height = 1200)
 #par(mfrow=c(1,2))
 agree_plot_rw<-agreementplot(obj1_rw$table, main = "Rwanda",xlab_rot=0, ylab_rot=90,xlab_just="center", ylab_just="center", xlab="Survey",ylab="Police")
 dev.off()
 
-tiff("/Users/joaovissoci/Desktop/figure2B.tiff", width = 700, height = 500,compression = 'lzw')
+tiff("/home/joao/Desktop/figure2B.tiff", units='in', width = 7, height = 6,compression = 'lzw',res=1200,bg = "white")
+postscript("/home/joao/Desktop/figure2B.eps",width = 1200, height = 1200)
 agree_plot_sl<-agreementplot(obj1_sl$table, main = "Sri-Lanka",xlab_rot=0, ylab_rot=90,xlab_just="center", ylab_just="center", xlab="Survey",ylab="Police")
 dev.off()
 
 #CI and Plotting
-tiff("/Users/joaovissoci/Desktop/figure3.tiff", width = 500, height = 500,compression = 'lzw')
+postscript("/home/joao/Desktop/figure3.eps",width = 1200, height = 1200)
+tiff("/home/joao/Desktop/figure3.tiff", units='in',width = 5, height = 5,compression = 'lzw',res=1200)#,bg="white")
 roc1 <- roc(agree_data_rw[,2],
             agree_data_rw[,1], percent=TRUE,
             # arguments for auc
