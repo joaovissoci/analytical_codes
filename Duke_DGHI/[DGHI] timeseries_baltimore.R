a057bc0e1079373b001a9691802dcaf32d9658a9
joaovissoci <joaovissoci@gmail.com>
2016-03-01 23:42:15 -0500

    updated wavelet baltimore analysis

--- Duke_DGHI/[DGHI] timeseries_baltimore.R
+++ Duke_DGHI/[DGHI] timeseries_baltimore.R
@@ -1,18 +1,21 @@
 
 data<-read.csv("/Users/joaovissoci/Desktop/Vulnerabilidade_pointsbypolygons.csv")
 
-temp_data<-with(data,data.frame(100000*(vuln2013/POP2010),
-	100000*(vulner2012/POP2010),
-	100000*(vulner2011/POP2010),
-	100000*(vulner2010/POP2010),
-	100000*(vulner2009/POP2010)))
+data<-read.csv("/home/joao/Desktop/Vulnerabilidade_pointsbypolygons.csv")
 
+
+temp_data<-with(data,c(1000*(vuln2013/POP2010),
+	1000*(vulner2012/POP2010),
+	1000*(vulner2011/POP2010),
+	1000*(vulner2010/POP2010),
+	1000*(vulner2009/POP2010)))
+year<-c(rep(200))
 
 #temp_data<-t(temp_data)
 #colnames(temp_data)<-c(1:200)
-time_series_data<-as.ts(temp_data)
+time_series_data<-as.ts(t(temp_data))
 
-timeseries_data <- ts(temp_data, frequency=200)
+timeseries_data <- ts(temp_data, start=c(2009,1),frequency=199)
 birthstimeseriescomponents <- decompose(timeseries_data)
 plot(birthstimeseriescomponents)
 
@@ -23,11 +26,11 @@ plot(fit)
 # additional plots
 monthplot(timeseries_data)
 library(forecast)
-seasonplot(timeseries_data)
+seasonplot(timeseries_data,col=rainbow(12),year.labels=TRUE)
 
 
 
 
-plot(time_series_data, type="b")
-diff12 = diff(time_series_data,12)
-acf2(diff12, 48)
+plot(timeseries_data, type="b")
+diff12 = diff(time_series_data)
+x<-acf(timeseries_data,type="partial",lag.max=199)
