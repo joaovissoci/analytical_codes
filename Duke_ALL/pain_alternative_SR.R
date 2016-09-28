64fbfa016b79d8cf42c38cb8b5fd3865dd649d0d
joaovissoci <joaovissoci@gmail.com>
2016-09-28 00:15:19 -0400

    standardized meta models for the pain alternative SR

--- Duke_ALL/pain_alternative_SR.R
+++ Duke_ALL/pain_alternative_SR.R
@@ -19,7 +19,7 @@ library, character.only=T)
 #IMPORTING DATA
 #################################################################
 #LOADING DATA FROM A .CSV FILE
-data<-read.csv("/Users/jnv4/Desktop/pain_outcomes.csv")
+data<-read.csv("/Users/joaovissoci/OneDrive - Duke University/datasets/Global EM/Pain_SR/pain outcome_SR.csv")
 #information between " " are the path to the directory in your computer where the data is stored
 
 #############################################################################
@@ -31,32 +31,30 @@ meta_model_var<-with(data,data.frame(
 									 intervention_cat,
 									 year,
 									 country,
-									 study_design,
 									 sample_size,
 									 intervention_1,
-									 contro_1,
-									 metanalysis,
-									 mean_pre,
-									 sd_pre,
-									 mean_post_1,
-									 sd_post_1,
+									 control_1,
+									 mean_pre_intervention,
+									 sd_pre_intervention,
+									 mean_post_intervention,
+									 sd_post_intervention,
 									 mean_pre_control,
 									 sd_pre_control,
-									 mean_post_1_control,
-									 sd_post_1_control,
-									 measure))
+									 mean_post_control,
+									 sd_post_control,
+									 Immediate))
 
-#extracting only studies with enough information for metanalysis
-meta_model<-subset(meta_model_var,
-	meta_model_var$metanalysis=="Yes")
+# #extracting only studies with enough information for metanalysis
+# meta_model<-subset(meta_model_var,
+# 	meta_model_var$metanalysis=="Yes")
 
 #############################################################################
 #Figure. 2
 #############################################################################
 ## Suicide ideation metanalysis model
-#extracting studies with suicide ideation measures
-meta_bssi<-subset(meta_model,
-	meta_model$measure=="100mm VAS")
+# #extracting studies with suicide ideation measures
+# meta_bssi<-subset(meta_model,
+# 	meta_model$measure=="100mm VAS")
 
 #exclude variables for prevalences instead of scale results
 # meta_bssi<-remove.vars(meta_bssi,c(
@@ -64,25 +62,25 @@ meta_bssi<-subset(meta_model,
 # 	"proportion_FUP1_control_group"))
 
 #excluding missing information
-meta_bssi<-na.omit(meta_bssi)
+# meta_bssi<-na.omit(meta_bssi)
 
-#Adjusting to avoind the error of a missing category in
-#the byvar analysis
-meta_bssi<-as.matrix(meta_bssi)
-meta_bssi<-as.data.frame(meta_bssi)
+# #Adjusting to avoind the error of a missing category in
+# #the byvar analysis
+# meta_bssi<-as.matrix(meta_bssi)
+# meta_bssi<-as.data.frame(meta_bssi)
 
-meta_bssi$intervention_1<-as.numeric(
-	as.character(meta_bssi$intervention_1)) 
-meta_bssi$mean_post_1<-as.numeric(
-	as.character(meta_bssi$mean_post_1))
-meta_bssi$sd_post_1<-as.numeric(
-	as.character(meta_bssi$sd_post_1))
-meta_bssi$contro_1<-as.numeric(
-	as.character(meta_bssi$contro_1))
-meta_bssi$mean_post_1_control<-as.numeric(
-	as.character(meta_bssi$mean_post_1_control))
-meta_bssi$sd_post_1_control<-as.numeric(
-	as.character(meta_bssi$sd_post_1_control))
+# meta_bssi$intervention_1<-as.numeric(
+# 	as.character(meta_bssi$intervention_1)) 
+# meta_bssi$mean_post_1<-as.numeric(
+# 	as.character(meta_bssi$mean_post_1))
+# meta_bssi$sd_post_1<-as.numeric(
+# 	as.character(meta_bssi$sd_post_1))
+# meta_bssi$contro_1<-as.numeric(
+# 	as.character(meta_bssi$contro_1))
+# meta_bssi$mean_post_1_control<-as.numeric(
+# 	as.character(meta_bssi$mean_post_1_control))
+# meta_bssi$sd_post_1_control<-as.numeric(
+# 	as.character(meta_bssi$sd_post_1_control))
 
 
 # #recoding metanalysis groups
@@ -93,13 +91,35 @@ meta_bssi$sd_post_1_control<-as.numeric(
 
 #run metanalysis model for continuous data
 meta1 <- metacont(intervention_1, 
-	mean_post_1,
-	sd_post_1,
-	contro_1,
-	mean_post_1_control,
-	sd_post_1_control, 
-  data=meta_bssi, sm="MD",
-  byvar=intervention_cat,print.byvar=FALSE,comb.fixed=FALSE)#,studlab=study
+	mean_post_intervention,
+	sd_post_intervention,
+	control_1,
+	mean_post_control,
+	sd_post_control, 
+  data=meta_model_var, sm="SMD",
+  byvar=intervention_cat,print.byvar=FALSE,
+  comb.fixed=FALSE,studlab=study)
+summary(meta1)
+
+tiff("/Users/jnv4/Desktop/figure1.tiff",
+  width = 800, height = 400,compression = 'lzw')
+forest(meta1)
+dev.off()
+
+funnel(meta1)
+metainf(meta1)
+metainf(meta1, pooled="random")
+
+#run metanalysis model for continuous data
+meta1 <- metacont(intervention_1, 
+	mean_post_intervention,
+	sd_post_intervention,
+	control_1,
+	mean_post_control,
+	sd_post_control, 
+  data=meta_model_var, sm="SMD",
+  byvar=Immediate,print.byvar=FALSE,
+  comb.fixed=FALSE,studlab=study)
 summary(meta1)
 
 tiff("/Users/jnv4/Desktop/figure1.tiff",
@@ -114,6 +134,77 @@ metainf(meta1, pooled="random")
 #############################################################################
 #Figure. 2
 #############################################################################
+
+# #extracting studies with suicide ideation measures
+ meta_model_direct<-subset(meta_model_var,
+ 	meta_model_var$intervention_cat=="Direct")
+
+#run metanalysis model for continuous data
+meta1 <- metacont(intervention_1, 
+	mean_post_intervention,
+	sd_post_intervention,
+	control_1,
+	mean_post_control,
+	sd_post_control, 
+  data=meta_model_physical, sm="SMD",
+  print.byvar=FALSE,
+  comb.fixed=FALSE,studlab=study)
+summary(meta1)
+
+forest(meta1)
+funnel(meta1)
+metainf(meta1)
+metainf(meta1, pooled="random")
+
+#exclude Kober
+
+# #extracting studies with suicide ideation measures
+ meta_model_indirect<-subset(meta_model_var,
+ 	meta_model_var$intervention_cat=="Indirect")
+
+#run metanalysis model for continuous data
+meta1 <- metacont(intervention_1, 
+	mean_post_intervention,
+	sd_post_intervention,
+	control_1,
+	mean_post_control,
+	sd_post_control, 
+  data=meta_model_indirect, sm="SMD",
+  print.byvar=FALSE,
+  comb.fixed=FALSE,studlab=study)
+summary(meta1)
+
+forest(meta1)
+funnel(meta1)
+metainf(meta1)
+metainf(meta1, pooled="random")
+
+#exclude Albert 2002
+
+# #extracting studies with suicide ideation measures
+ meta_model_physical<-subset(meta_model_var,
+ 	meta_model_var$intervention_cat=="Physical")
+
+#run metanalysis model for continuous data
+meta1 <- metacont(intervention_1, 
+	mean_post_intervention,
+	sd_post_intervention,
+	control_1,
+	mean_post_control,
+	sd_post_control, 
+  data=meta_model_physical, sm="SMD",
+  print.byvar=FALSE,
+  comb.fixed=FALSE,studlab=study)
+summary(meta1)
+
+forest(meta1)
+funnel(meta1)
+metainf(meta1)
+metainf(meta1, pooled="random")
+
+#exclude Lau, Chow, and Pope 2008 
+
+
 ## Suicide ideation metanalysis model
 #extracting studies with suicide ideation measures
 meta_bssi<-subset(meta_model,
