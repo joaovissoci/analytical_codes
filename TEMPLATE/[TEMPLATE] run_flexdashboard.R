8878ac451971a247cd1e3a94b1c17a5d6ee225b5
Joao Vissoci <joaovissoci@gmail.com>
2017-12-14 23:32:25 -0500

    dashboard changes

--- TEMPLATE/[TEMPLATE] run_flexdashboard.R
+++ TEMPLATE/[TEMPLATE] run_flexdashboard.R
@@ -1,6 +1,8 @@
 install.packages(c("shiny", "dplyr", "htmlwidgets", "digest", "bit"))
 devtools::install_github("jcheng5/bubbles")
 devtools::install_github("hadley/shinySignals")
-install.packages("flexdashboard")
+devtools::install_github(c('yihui/knitr', 'rstudio/flexdashboard'))
+devtools::install_github("rstudio/rmarkdown")
 
-rmarkdown::run("/Users/Joao/Git/analytical_codes/Duke_DGNN/US_SAP_faculty_dashboard.rmd")
+
+rmarkdown::run("/Users/joaovissoci/Git/analytical_codes/Duke_DGNN/US_SAP_faculty_dashboard.rmd")
